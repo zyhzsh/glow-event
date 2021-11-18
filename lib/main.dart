c994cd6dbd688ddaac98a01edc5b78895eb38cd4
Luca Bulles <lucabulles@gmail.com>
2021-11-18 11:56:13 +0100

    added comment for version control in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:glow2021v1/screens/intro_screen.dart';
 
 void main() => runApp(App());
-
+//This version is used at Glow 2021 for the artwork of Atelier Vrijdag
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
   @override
